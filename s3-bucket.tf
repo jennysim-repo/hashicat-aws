f2fa92a634c1d8828fac02cbb1a06e78269e4c7b
jaeheesim <jaehee1440@gmail.com>
2023-12-22 08:11:59 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,11 @@
 module "s3-bucket" {
   source  = "app.terraform.io/hashicorp-chip/s3-bucket/aws"
   version = "2.8.0"
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+  enabled = true
 }
